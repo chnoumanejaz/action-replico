bbd05164cb2638d56b8dabdd52719f90cb300e62
chnoumanejaz <noumanejaz92@gmail.com>
2024-02-03 02:14:34 +0500

    [fix] font build error resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 });
