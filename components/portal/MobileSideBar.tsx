0d8869a3d4f17ce9c7b15c61a8c3c95b376390e1
chnoumanejaz <noumanejaz92@gmail.com>
2024-02-05 16:01:23 +0500

    [functionality] mobile sidebar added

---
+++ components/portal/MobileSideBar.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { SidebarMenu } from '@/data/sidebar-data';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import Logo from '../common/Logo';
+import LogoutIcon from '../common/LogoutIcon';
+import { ThemeToggler } from '../common/ThemeToggler';
+import UserAccount from '../common/UserAccount';
+import UserAccountType from '../common/UserAccountType';
+import { Button } from '../ui/button';
+
+const MobileSideBar = () => {
+  const pathname = usePathname();
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button asChild size="icon" variant="outline" className="p-1">
+          <HamburgerMenuIcon className="h-4 w-4" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>
+            <Logo />
+          </SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col justify-between ">
+          <div className="flex flex-col my-2 py-6 space-y-2 ">
+            {SidebarMenu.map(menu => {
+              const { icon: Icon } = menu;
+              return (
+                <Link
+                  href={menu.path}
+                  key={menu.id}
+                  className={`flex shadow-sm border border-secondary/25 rounded-md py-2 px-3 items-center font-medium group dark:hover:bg-primary/50 hover:bg-secondary transition ${
+                    pathname === menu.path
+                      ? 'dark:bg-primary/50 bg-secondary'
+                      : ''
+                  }`}>
+                  <Icon className="w-5 h-5 mr-2 group-hover:rotate-90 transition duration-300" />
+                  {menu.name}
+                </Link>
+              );
+            })}
+          </div>
+
+          <div className="border pt-3 pb-6 rounded-2xl px-4 overflow-hidden -m-3 shadow-md">
+            <UserAccount />
+            <div className="mt-3 flex gap-2 justify-between">
+              <UserAccountType />
+              <div className="space-x-2">
+                <ThemeToggler />
+                <LogoutIcon />
+              </div>
+            </div>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSideBar;
--- components/portal/PageWrapper.tsx
+++ components/portal/PageWrapper.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MobileSideBar from './MobileSideBar';
 
 interface PageWrapperProps {
   children: React.ReactNode;
@@ -13,8 +14,15 @@ const PageWrapper: React.FC<PageWrapperProps> = ({
 }) => {
   return (
     <div className="h-full">
-      <h2 className="text-2xl  font-semibold">{title}</h2>
-      <p className="mb-10 text-muted-foreground text-sm">{subheading}</p>
+      <div className="flex justify-between gap-2">
+        <div>
+          <h2 className="text-2xl  font-semibold">{title}</h2>
+          <p className="mb-10 text-muted-foreground text-sm">{subheading}</p>
+        </div>
+        <div className="mt-2 md:hidden">
+          <MobileSideBar />
+        </div>
+      </div>
       {children}
     </div>
   );
